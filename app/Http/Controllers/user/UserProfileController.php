2f4b9431570c4ca46d9e4acf5602438494b0fa9e
Jknoorisys <javeriya.noorisys@gmail.com>
2023-09-15 11:12:03 +0530

    User get profile

---
+++ app/Http/Controllers/user/UserProfileController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\user;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class UserProfileController extends Controller
+{
+    public function getProfile(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            
+            'user_id'   => ['required','alpha_dash', Rule::notIn('undefined')],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'status'    => 'failed',
+                    'errors'    =>  $validator->errors(),
+                    'message'   =>  __('msg.validation'),
+                ],
+                400
+            );
+        }
+
+        try {
+            $user = DB::table('users')->where('id', '=', $req->user_id)->first();
+            if (!empty($user)) {
+                return response()->json(
+                    [
+                        'status'    => 'success',
+                        'data' => $user,
+                        'message'   =>  __('msg.profile.uprofile'),
+                    ],
+                    200
+                );
+            } else {
+                return response()->json(
+                    [
+                        'status'    => 'failed',
+                        'message'   =>  __('msg.profile.usernotfound'),
+                    ],
+                    400
+                );
+            }
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status'  => 'failed',
+                'message' =>  __('msg.error'),
+                'error'   => $e->getMessage()
+            ], 500);
+        }
+    }
+}
